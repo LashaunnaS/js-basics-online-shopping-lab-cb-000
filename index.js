d502630178f25915d41e3735b0325460d4c8bc69
Lashaunna Samuels <shaunna191@hotmail.com>
2017-12-11 21:16:49 -0500

    Completed addToCart() and fiewCart()

--- index.js
+++ index.js
@@ -10,11 +10,34 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let itemPrice = Math.floor(Math.random() * 100)+1;
+  cart.push({[item]: itemPrice});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  let cartList = []; 
+  if(cart == ''){
+    return console.log(`Your shopping cart is empty.`);
+  } 
+  for (let i=0; i < cart.length; i++) {
+    let objectInCart = cart[i]; //This will return the k:v pair
+    let item = (Object.keys(objectInCart))[0]; //This will return the key
+    let price = objectInCart[item]; //This wil return the value 
+    cartList.push(`${item} at $${price}`); 
+  }
+  switch(cart.length) {
+    case 1:
+        break;
+    case 2:
+        cartList = cartList.join(' and ');
+        break;
+    default:
+        cartList[cart.length - 1] = "and ".concat(cartList[cart.length - 1]);
+        cartList = cartList.join(', ');
+  }
+   console.log(`In your cart, you have ${cartList}.`);
 }
 
 function total() {
